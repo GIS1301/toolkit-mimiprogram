b72717a0d3cfb747cfe1c44d3ab10f68f2d0e182
yangfan2016 <15234408101@163.com>
2018-11-24 23:51:58 +0800

    fix: new version

---
+++ components/gis-input.js
@@ -0,0 +1,116 @@
+// components/gis-input.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        propType: {
+            type: String,
+            value: "lon", // 经度
+        },
+        propDeg: {
+            type: Object,
+            value() {
+                return {
+                    deg: '',
+                    min: '',
+                    sec: '',
+                };
+            },
+            observer(newVal, oldVal, changedPath) {
+                let { deg, min, sec } = newVal;
+                this.setData({
+                    deg,
+                    min,
+                    sec,
+                });
+            }
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        isLon: true,
+        labelText: "经度",
+        // input
+        deg: "", // 度
+        min: "", // 分
+        sec: "", // 秒
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        // v-modal
+        changeVal(ev) {
+            let {
+                target,
+                detail
+            } = ev;
+            let type = target.dataset.type;
+            let val = detail.value;
+            let isValid = this.detectRange(type, val);
+            if (!isValid) {
+                val = "";
+            }
+            this.setData({
+                [type]: val,
+            });
+        },
+        detectRange(type, value) {
+            let mp = this;
+            let latRange = mp.data.isLon ? 180 : 90;
+            if (value < 0) return false;
+            let val = Math.abs(value);
+            let mode = {
+                "deg": function(v) {
+                    let range = latRange;
+                    return v <= range;
+                },
+                "min": function(v) {
+                    let range = 60;
+                    return v <= range;
+                },
+                "sec": function(v) {
+                    let range = 60;
+                    return v <= range;
+                }
+            };
+            let fn = mode[type];
+            if (!fn) return false;
+            return fn(val);
+        },
+        postMsgToParent() {
+            let {
+                deg,
+                min,
+                sec
+            } = this.data;
+            this.triggerEvent("set-coords", {
+                deg,
+                min,
+                sec,
+            });
+        }
+    },
+    lifetimes: {
+        attached() {
+            let isLon = this.properties.propType === "lon";
+            let {
+                deg,
+                min,
+                sec
+            } = this.properties.propDeg;
+            this.setData({
+                isLon: isLon,
+                labelText: isLon ? "经度" : "纬度",
+                deg,
+                min,
+                sec,
+            });
+        }
+    }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,14 +4,67 @@ const app = getApp()
 
 const Tool = require("./calc.js");
 
+const DEFAULT_DEG=function () {
+    return {
+        deg: '',
+        min: '',
+        sec: '',
+    };
+};
+const DEFAULT_ANG ="0°0'0''";
+
 Page({
     data: {
-        lonA: 0,
-        latA: 0,
-        lonB: 0,
-        latB: 0,
+        isShowResult:true,
+        lonA: DEFAULT_DEG(),
+        latA: DEFAULT_DEG(),
+        lonB: DEFAULT_DEG(),
+        latB: DEFAULT_DEG(),
         angle: 0,
         dis: 0,
+        uiAngle: DEFAULT_ANG,
+        copyAngle:'0',
+    },
+    // 复制
+    copy2ClipBoard({target}) {
+        let txt=(target.dataset.copy)+"";
+        wx.setClipboardData({
+            data: txt,
+        })
+    },
+    // 转换度-》度分秒
+    d2dms(d) {
+        let deg = d | 0;
+        let temp = (d - deg) * 60;
+        let min = temp | 0;
+        let sec = (temp - min) * 60;
+        return {
+            deg,
+            min,
+            sec,
+        };
+    },
+    // 转换度分秒-》度
+    dms2d({
+        deg,
+        min,
+        sec
+    }) {
+        return (+deg) + (+min) / 60 + (+sec) / 3600;
+    },
+    // 获取坐标
+    setCoords(ev) {
+        let {
+            detail,
+            target
+        } = ev;
+        let key = target.dataset.key;
+        if (JSON.stringify(detail) !== JSON.stringify(this.data[key])) {
+            console.count("emit");
+            this.setData({
+                [key]: detail,
+            });
+        }
     },
     //事件处理函数
     calcDisAndAngle() {
@@ -21,12 +74,18 @@ Page({
             lonB,
             latB
         } = this.data;
-        
+
+        // 转换成度
+        lonA = this.dms2d(lonA);
+        latA = this.dms2d(latA);
+        lonB = this.dms2d(lonB);
+        latB = this.dms2d(latB);
+
         if (!this.isNumber(lonA) || !this.isNumber(latA) || !this.isNumber(lonB) || !this.isNumber(latB)) {
             return wx.showModal({
                 title: '提示',
                 content: '请输入合法数字',
-                complete:()=> {
+                complete: () => {
                     this.resetCalcResult();
                 },
             });
@@ -41,19 +100,31 @@ Page({
         }
         let angle = Tool.getAngleBetweenTwoPoints(lonA, latA, lonB, latB);
         let dis = Tool.getDistanceBetweenTwoPoints(lonA, latA, lonB, latB);
+        let copyAngle = angle;
+        // 转成度分秒
+        angle=this.d2dms(angle);
+        angle.sec = (+angle.sec).toFixed(5);
+        let uiAngle=`${angle.deg}°${angle.min}'${angle.sec}''`;
+
         this.setData({
             angle,
             dis,
+            isShowResult:true,
+            copyAngle: copyAngle,
+            uiAngle: uiAngle,
         });
     },
     resetCalcResult() {
         this.setData({
-            lonA: 0,
-            latA: 0,
-            lonB: 0,
-            latB: 0,
+            isShowResult:false,
+            lonA: DEFAULT_DEG(),
+            latA: DEFAULT_DEG(),
+            lonB: DEFAULT_DEG(),
+            latB: DEFAULT_DEG(),
             angle: 0,
             dis: 0,
+            uiAngle: DEFAULT_ANG,
+            copyAngle:"0",
         });
     },
     changeInput({
@@ -73,21 +144,17 @@ Page({
     isNumber(val) {
         return +val == val;
     },
-    onLoad() {
-
-    },
     onHide() {
         // 传递数据
         wx.setStorage({
             key: "yan-pos",
-            data: [
-                {
-                    latitude:this.data.latA,
-                    longitude:this.data.lonA
+            data: [{
+                    latitude: this.dms2d(this.data.latA),
+                    longitude:this.dms2d( this.data.lonA)
                 },
                 {
-                    latitude: this.data.latB,
-                    longitude: this.data.lonB
+                    latitude: this.dms2d(this.data.latB),
+                    longitude: this.dms2d(this.data.lonB)
                 }
             ]
         })
