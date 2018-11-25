0961f55c38e2abd9096cc35fa10899ed7171ad48
yangfan2016 <15234408101@163.com>
2018-11-25 16:11:36 +0800

    fix: page code structor

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,162 +1,64 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
-const Tool = require("./calc.js");
-
-const DEFAULT_DEG=function () {
-    return {
-        deg: '',
-        min: '',
-        sec: '',
-    };
-};
-const DEFAULT_ANG ="0°0'0''";
-
 Page({
+
+    /**
+     * 页面的初始数据
+     */
     data: {
-        isShowResult:true,
-        lonA: DEFAULT_DEG(),
-        latA: DEFAULT_DEG(),
-        lonB: DEFAULT_DEG(),
-        latB: DEFAULT_DEG(),
-        angle: 0,
-        dis: 0,
-        uiAngle: DEFAULT_ANG,
-        copyAngle:'0',
-    },
-    // 复制
-    copy2ClipBoard({target}) {
-        let txt=(target.dataset.copy)+"";
-        wx.setClipboardData({
-            data: txt,
-        })
-    },
-    // 转换度-》度分秒
-    d2dms(d) {
-        let deg = d | 0;
-        let temp = (d - deg) * 60;
-        let min = temp | 0;
-        let sec = (temp - min) * 60;
-        return {
-            deg,
-            min,
-            sec,
-        };
-    },
-    // 转换度分秒-》度
-    dms2d({
-        deg,
-        min,
-        sec
-    }) {
-        return (+deg) + (+min) / 60 + (+sec) / 3600;
-    },
-    // 获取坐标
-    setCoords(ev) {
-        let {
-            detail,
-            target
-        } = ev;
-        let key = target.dataset.key;
-        if (JSON.stringify(detail) !== JSON.stringify(this.data[key])) {
-            console.count("emit");
-            this.setData({
-                [key]: detail,
-            });
-        }
-    },
-    //事件处理函数
-    calcDisAndAngle() {
-        let {
-            lonA,
-            latA,
-            lonB,
-            latB
-        } = this.data;
 
-        // 转换成度
-        lonA = this.dms2d(lonA);
-        latA = this.dms2d(latA);
-        lonB = this.dms2d(lonB);
-        latB = this.dms2d(latB);
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
-        if (!this.isNumber(lonA) || !this.isNumber(latA) || !this.isNumber(lonB) || !this.isNumber(latB)) {
-            return wx.showModal({
-                title: '提示',
-                content: '请输入合法数字',
-                complete: () => {
-                    this.resetCalcResult();
-                },
-            });
-        }
+    },
 
-        if (lonA === 0 && latA === 0 && lonB === 0 && latB === 0) {
-            return wx.showModal({
-                title: '提示',
-                content: '无效输入',
-                showCancel: false,
-            });
-        }
-        let angle = Tool.getAngleBetweenTwoPoints(lonA, latA, lonB, latB);
-        let dis = Tool.getDistanceBetweenTwoPoints(lonA, latA, lonB, latB);
-        let copyAngle = angle;
-        // 转成度分秒
-        angle=this.d2dms(angle);
-        angle.sec = (+angle.sec).toFixed(5);
-        let uiAngle=`${angle.deg}°${angle.min}'${angle.sec}''`;
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-        this.setData({
-            angle,
-            dis,
-            isShowResult:true,
-            copyAngle: copyAngle,
-            uiAngle: uiAngle,
-        });
     },
-    resetCalcResult() {
-        this.setData({
-            isShowResult:false,
-            lonA: DEFAULT_DEG(),
-            latA: DEFAULT_DEG(),
-            lonB: DEFAULT_DEG(),
-            latB: DEFAULT_DEG(),
-            angle: 0,
-            dis: 0,
-            uiAngle: DEFAULT_ANG,
-            copyAngle:"0",
-        });
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
     },
-    changeInput({
-        detail,
-        target
-    }) {
-        let {
-            value
-        } = detail;
-        let {
-            key
-        } = target.dataset;
-        this.setData({
-            [key]: value,
-        });
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
     },
-    isNumber(val) {
-        return +val == val;
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
     },
-    onHide() {
-        // 传递数据
-        wx.setStorage({
-            key: "yan-pos",
-            data: [{
-                    latitude: this.dms2d(this.data.latA),
-                    longitude:this.dms2d( this.data.lonA)
-                },
-                {
-                    latitude: this.dms2d(this.data.latB),
-                    longitude: this.dms2d(this.data.lonB)
-                }
-            ]
-        })
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
     }
 })
--- pages/map/index.js
+++ pages/map/index.js
@@ -3,26 +3,29 @@ Page({
         lat: 0, // 纬度
         lon: 0,
     },
+    onLoad(opts) {
+        console.log(opts);
+    },
     onShow() {
         let mp=this;
-        wx.getStorage({
-            key: 'yan-pos',
-            success: function({data}) {
-                mp.setData({
-                    polyline: [{
-                        points:data,
-                        color: "#ff0022",
-                        width: 2,
-                        dottedLine: true
-                    }]
-                },function () {
-                    // 缩放视野
-                    let map = wx.createMapContext('yanMap', this);
-                    map.includePoints({
-                        points: mp.data.polyline[0].points,
-                    });
-                });
-            },
-        })
+        // wx.getStorage({
+        //     key: 'yan-pos',
+        //     success: function({data}) {
+        //         mp.setData({
+        //             polyline: [{
+        //                 points:data,
+        //                 color: "#ff0022",
+        //                 width: 2,
+        //                 dottedLine: true
+        //             }]
+        //         },function () {
+        //             // 缩放视野
+        //             let map = wx.createMapContext('yanMap', this);
+        //             map.includePoints({
+        //                 points: mp.data.polyline[0].points,
+        //             });
+        //         });
+        //     },
+        // })
     }
 })
